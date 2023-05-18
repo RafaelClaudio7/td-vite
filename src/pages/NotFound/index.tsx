6876fc3f48becf2b2c0db430234fb7542ea1b0f8
RafaelClaudio7 <rc7true@gmail.com>
2023-05-18 18:20:39 -0300

    color ajustee

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -10,7 +10,7 @@ export default function NotFound() {
 
   return (
     <Center h="100vh" w="100vw" bg={colors.black} flexDir="column">
-      <Text color={colors.greenLight} fontSize={"32px"} fontFamily={"Roboto"}>
+      <Text color={colors.white} fontSize={"32px"} fontFamily={"Roboto"}>
         Página não encontrada!
       </Text>
       <img src={notFound} alt="Não encontrado" height={350} width={350} />
@@ -19,14 +19,19 @@ export default function NotFound() {
         onClick={() => navigation("/")}
         cursor={"pointer"}
       >
-        <ArrowBackIcon color={colors.greenLight} boxSize={8} />
+        <ArrowBackIcon
+          color={colors.white}
+          boxSize={8}
+          _hover={{ color: colors.green }}
+        />
         <Text
-          color={colors.greenLight}
+          color={colors.white}
           display={"inline"}
           ml={5}
           fontSize={"20px"}
           fontFamily={"Roboto"}
           fontWeight={300}
+          _hover={{ color: colors.green }}
         >
           Voltar para home
         </Text>
